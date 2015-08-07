edc52d482d52e2870829c619cff140094c5737a8
xiaozhenxing <xiaozhenxing@qq.com>
2015-08-07 09:25:13 +0800

    update test

--- src/com/test.java
+++ src/com/test.java
@@ -7,7 +7,7 @@ public class test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("git start....");
+		System.out.println("git start................");
 	}
 	
 }
